6312ec81e8060eee29f55977ded0721b345a3176
Obsidan <Filliross@gmail.com>
2026-01-15 16:18:31 +0100

    Finished scaffold

--- server.mjs
+++ server.mjs
@@ -1,22 +1,13 @@
 import express from "express";
 
-//TODO learn how this app object works and finish the scaffold
-
-
-const PORT = system.env.PORT || 8080;
-//The server is listening to the port.
-//  A port is an adress or entrance. 8080, FTP is 21. File transfer protocol. 8080 is a port for http. It's a convention.
-const app = new express();
-//HTTP verb for fetching information
-
-app.use(express.static("public"))
+const app = express()
+const port = 8080
 
 app.get('/', (req, res) => {
-    res.send("Hello world")
+  res.send('Hello World!')
 })
 
-app.listen(PORT, () => {
-    console.log("listening on port ", PORT)
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
 
-//index.html is where the requests get sent from.
